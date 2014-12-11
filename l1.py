1c7a956925b0263290f1c2dff8cad8db11db10dc
juyuan.liu <liujuyuan@gmail.com>
2014-12-11 21:33:20 +0000

    added stopping criteria: any coordinate descent leads to a change in objective function smaller than epsilon.

--- l1.py
+++ l1.py
@@ -9,15 +9,18 @@ A[:,[x for x in range(3) if x != 1]]
 
 
 # l1
-def opt_l1(A, b, _lambda, epsilon):
+def opt_l1(A, b, _lambda, epsilon=1e-7, maxit=100000):
 	'''minimizes ||Ax-b||_2 + lambda*||x||_1'''
 	# initial x
 	d = A.shape[1]
 	x = np.zeros(d)
-	converged = False
-	while np.linalg.norm(A.dot(x)-b) > epsilon:
+	notConverged = True
+	iteration = 0
+	while notConverged:
+		notConverged = True and (iteration<= maxit)
+		currentOpt = np.linalg.norm(A.dot(x))**2+_lambda*np.linalg.norm(x,1)
 		for i in range(d):
-		# 2*A{i}'*((A{-i}*x{-i}+A{i}x{i}-y)) + delta|x{i}| = 0
+		# 2*A{i}'*((A{-i}*x{-i}+A{i}x{i}-b)) + delta|x{i}| = 0
 			A_negI = A[:,[idx for idx in range(d) if idx!=i]]
 			A_i = A[:,i]
 			x_negI = x[[idx for idx in range(d) if idx!=i]]
@@ -25,10 +28,15 @@ def opt_l1(A, b, _lambda, epsilon):
 			y = A_i.dot(b-A_negI.dot(x_negI))/A_i.dot(A_i)
 			if y > t:
 				x[i] = y-t
-			elif y < t:
+			elif y < -t:
 				x[i] = y+t
 			else:
 				x[i] = 0
+			newOpt = np.linalg.norm(A.dot(x))**2+_lambda*np.linalg.norm(x,1)
+			currentOpt = newOpt
+			if abs(newOpt-currentOpt) < epsilon:
+				notConverged=False
+		iteration += 1
 		print x
 		print np.linalg.norm(A.dot(x)-b,2)			
 	return x
